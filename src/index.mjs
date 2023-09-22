cf8bbceca39b76e245da5adc236d4ad31ee9d66f
エノモト <120731888+kosuke-enomoto@users.noreply.github.com>
2023-09-22 14:26:45 +0000

    Update 4 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,10 @@
 import "./styles.css";
 
+console.log("test");
+
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
+
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
